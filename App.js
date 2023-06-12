056e3514b91651496ad9a83dc660eab613c08992
jayeshsendre <126330978+jayeshsendre@users.noreply.github.com>
2023-06-12 18:38:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Quiz from "./quiz";
+
+
+
+export default function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
---
+++ QuizData.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    questionText: "How many days are there in a week?",
+    answerOptions: [
+      { answerText: "2", isCorrect: false },
+      { answerText: "5", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+      { answerText: "10", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is your developer of this project?",
+    answerOptions: [
+      { answerText: "Jayesh", isCorrect: true },
+      { answerText: "John", isCorrect: false },
+      { answerText: "Aman", isCorrect: false },
+      { answerText: "Manas", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is React?",
+    answerOptions: [
+      {
+        answerText: "A JavaScript library for building user interfaces.",
+        isCorrect: true,
+      },
+      { answerText: "A database management system.", isCorrect: false },
+      { answerText: "A text editor.", isCorrect: false },
+      { answerText: "A programming language.", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is JSX in React?",
+    answerOptions: [
+      {
+        answerText:
+          "A JavaScript extension that allows you to write HTML-like syntax in your JavaScript code.",
+        isCorrect: true,
+      },
+      {
+        answerText: "A programming language used for server-side scripting.",
+        isCorrect: false,
+      },
+      { answerText: "A CSS preprocessor.", isCorrect: false },
+      { answerText: "A version control system.", isCorrect: false },
+    ],
+  },
+];
+export default questions
